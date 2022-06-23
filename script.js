0c1f41833eb5daf608f08b122028b11bc088183d
Your Name <you@example.com>
2022-06-23 20:20:30 +0100

    inserted space

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const myName='Christiana';
 const country='Nigeria';
 const height= 5.6;
 
-alert("My Name is "+myName+ ".\n "+"I am from"+country+ ". \n" +"My height is "+height);
+alert("My Name is "+myName+ ".\n "+"I am from "+country+ ". \n" +"My height is "+height);
 
